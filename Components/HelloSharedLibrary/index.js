777004ff4c9b04fa92efc5499136e9278630f894
Konrad Cendrowski <konc@netcompany.com>
2022-08-10 15:26:00 +0200

    .

--- Components/HelloSharedLibrary/index.js
+++ Components/HelloSharedLibrary/index.js
@@ -1,7 +1,7 @@
-import {Text, View} from "react-native";
-import React from "react";
-
-export const HelloSharedLibrary = () => {
-    console.log("Hello shared-library!!!....")
-    return (<Text>Hello shared-library!!!</Text>)
-}
+// import {Text, View} from "react-native";
+// import React from "react";
+//
+// export const HelloSharedLibrary = () => {
+//     console.log("Hello shared-library!!!....")
+//     return (<Text>Hello shared-library!!!</Text>)
+// }
--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 //empty index.js
+
+
+import {Text, View} from "react-native";
+import React from "react";
+
+export const HelloSharedLibrary = () => {
+    console.log("Hello shared-library!!!....")
+    return (<Text>Hello shared-library!!!</Text>)
+}
