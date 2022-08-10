13293e2916b0eaa168561a3fd27d66ac59bfd285
Konrad Cendrowski <konc@netcompany.com>
2022-08-10 15:02:35 +0200

    Clean up

---
+++ Components/HelloSharedLibrary/index.js
@@ -0,0 +1,9 @@
+import {Text, View} from "react-native";
+import React from "react";
+
+export const HelloSharedLibrary = () => {
+    console.log("Hello shared-library!!!....")
+    return <View>
+        <Text>Hello shared-library!!!</Text>
+    </View>
+}
