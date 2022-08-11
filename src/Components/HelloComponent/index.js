4794a27eb537e979a64f8586b1d1b88168308f2b
Konrad Cendrowski <konc@netcompany.com>
2022-08-11 09:22:09 +0200

    Change jsx to js

---
+++ src/Components/HelloComponent/index.js
@@ -0,0 +1,6 @@
+import {Text} from "react-native";
+import React from "react";
+
+const HelloComponent = () => {
+    return /*#__PURE__*/React.createElement(Text, null, "Hello from shared library!!!");
+};
