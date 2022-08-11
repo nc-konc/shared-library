178892fac620c6d53728c6b7203a77fed7b44e81
Konrad Cendrowski <konc@netcompany.com>
2022-08-11 10:30:15 +0200

    Add input param

--- src/Components/HelloComponent/index.js
+++ src/Components/HelloComponent/index.js
@@ -1,7 +1,7 @@
 import {Text} from "react-native";
 import React from "react";
-
-export const HelloComponent = (text) => {
+{}
+export const HelloComponent = ({text}) => {
     console.log("Hello shared-library!!!....")
     return (<Text>{text}</Text>)
 }
