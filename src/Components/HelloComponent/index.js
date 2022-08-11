c2ce3077a055b18963427bfc5f7d254f2502c82d
Konrad Cendrowski <konc@netcompany.com>
2022-08-11 11:22:53 +0200

    Update

--- src/Components/HelloComponent/index.js
+++ src/Components/HelloComponent/index.js
@@ -1,7 +1,18 @@
 import {Text} from "react-native";
 import React from "react";
 {}
-export const HelloComponent = ({text}) => {
+export const HelloComponent = ({text, outerStyles}) => {
     console.log("Hello shared-library!!!....")
-    return (<Text>{text}</Text>)
+    const styles = StyleSheet.compose(innerStyles.text, outerStyles.text)
+    return (<Text style={styles.text}>{text}</Text>)
 }
+
+const innerStyles = StyleSheet.create(
+    {
+        text: {
+            backgroundColor: "red",
+            fontSize: 20,
+            fontStyle: "italic"
+        }
+    }
+)
