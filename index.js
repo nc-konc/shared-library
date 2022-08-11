328d5767d14b5f3b779314b125f9c437302050d6
Konrad Cendrowski <konc@netcompany.com>
2022-08-11 10:11:22 +0200

    Changes

---
+++ index.js
@@ -0,0 +1,3 @@
+import {HelloComponent} from "./src/Components/HelloComponent";
+
+export default HelloComponent
--- src/Components/HelloComponent/index.js
+++ src/Components/HelloComponent/index.js
@@ -2,5 +2,6 @@ import {Text} from "react-native";
 import React from "react";
 
 export const HelloComponent = () => {
-    return /*#__PURE__*/React.createElement(Text, null, "Hello from shared library!!!");
-};
+    console.log("Hello shared-library!!!....")
+    return (<Text>Hello shared-library!!!</Text>)
+}
